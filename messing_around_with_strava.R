643f841628a248199fa96ff845a0d0ace32454c9
Jacob Rozran <jacobrozran@ip-10-11-8-171.ec2.internal>
2020-03-11 14:25:43 -0400

    still think I cannot access non-Jake activities except for some clipped stuff from the club

--- messing_around_with_strava.R
+++ messing_around_with_strava.R
@@ -10,7 +10,7 @@ secret <- 'MY_SECRET_KEY'
 ## CREATE YOUR STRAVA TOKEN
 token <- httr::config(token = strava_oauth(name, client_id, secret, 
                                            cache = TRUE, 
-                                           app_scope = "read,read_all,activity:read,activity:read_all,profile:read_all")) 
+                                           app_scope = "activity:read_all")) 
 
 stoken <- httr::config(token = readRDS('.httr-oauth')[[1]])
 
@@ -19,3 +19,11 @@ stoken <- httr::config(token = readRDS('.httr-oauth')[[1]])
 #JEANA -> 51784376
 
 myinfo <- get_athlete(stoken, id = '6034935')
+jake <- get_activity_list(stoken)
+
+for(i in 1:260){
+    print(jake[[i]]$athlete$id)
+}
+
+rdc <- get_club(stoken, id = '592304', request = "activities")
+as.data.frame(rdc)
